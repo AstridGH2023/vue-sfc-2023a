f5f0cc38d6cb2c28b4fa594385e9efdd9210f1a7
AstridGH2023 <122814677+AstridGH2023@users.noreply.github.com>
2023-01-19 16:17:08 +0000

    carga de datos.json

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,24 @@
+<script>
+import sourceData from '@/data.json'
+console.log();
+export default{
+  data(){
+    return{
+      destinations: sourceData.destinations
+    }
+  }
+}
+</script>
 <template>
     <div>
-      <h1>Home</h1>
+      <h1>Todos los destinos</h1>
+      <div class="destinations"></div>
+      <RouterLink 
+      v-for="destination in destinations" 
+      :key="destination.id"
+      :to="destination.slug">
+      <h2>{{ destination.name }}</h2>
+      <img :src="`images/${destination.image}`" :alt="destination.name">
+    </RouterLink>
     </div>
   </template>
